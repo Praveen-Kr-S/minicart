5223e50b3162e91bcd282b9013ad2f847def9ec9
PraveenKumar S <praveenkumar328be@gmail.com>
2024-01-27 19:42:41 +0530

    Initial code commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,32 @@
+const ProductModel = require("../models/productModel")
+
+//Get Products API - /api/v1/produts
+exports.getProducts = async (req, res, next) => {
+    const query = req.query.keyword?{ name : { 
+        $regex: req.query.keyword,
+        $options: 'i'
+     }}:{}
+    const products = await ProductModel.find(query);
+    res.json({
+        success: true,
+        products
+    })
+}
+
+//Get Single Produect API - /api/v1/product/:id
+exports.getSingleProducts = async (req, res, next) => {
+
+    try{
+        const product = await ProductModel.findById(req.params.id);
+        res.json({
+            success: true,
+            product
+        })
+    } catch(error){
+        res.status(404).json({
+            success: false,
+            /* message: error.message */
+            message: "Unable to get Product with that ID"
+        })
+    }
+}
